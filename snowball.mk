d254ff4256e1e6fd8c4da58819b70422bd62315d
Amit Pundir <amit.pundir@linaro.org>
2011-12-12 19:25:18 +0530

    mali snowball integration
    Change-Id: I7a720142c46df3707310b2ad45b75e9ed96cd6ec
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- snowball.mk
+++ snowball.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/linaro/common/common.mk)
 $(call inherit-product, device/linaro/snowball/device.mk)
+$(call inherit-product-if-exists, vendor/st-ericsson/snowball/device-snowball.mk)
 
 PRODUCT_BRAND := snowball
 PRODUCT_DEVICE := snowball
