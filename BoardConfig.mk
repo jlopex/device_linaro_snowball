d246509858748046d725ac6d4b3e47bc31e701e8
Patrik Ryd <patrik.ryd@stericsson.com>
2012-03-28 10:02:46 +0200

    Configure build to be able to build multimedia from ST-Ericsson
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>
    Committed-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    
    cherry picked from commit c869ad0617d7f920636739a08461a29fbe269c7b
    
    Change-Id: Icbe1e1f64e5fd58208af6243d3a721c01bae32cf
    Signed-off-by: Patrik Ryd <patrik.ryd@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,3 +50,35 @@ TARGET_MALI_DRIVER_DIR := drivers/gpu/mali/mali400ko/driver/src/devicedrv/mali
 
 # Snowball uses a Nova A9500 -- Dual-Core Cortex A9
 TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9,$(call cc-option,-mtune=cortex-a8)) $(call cc-option,-mcpu=cortex-a9,$(call cc-option,-mcpu=cortex-a8))
+
+#Need these for MM build
+LIBBASSAPP_ENABLE_FEATURE_LIBBASSAPP := false
+CAMERA_SET_PRIMARY_SENSOR ?= IMX072
+STE_CAMERA_ENABLE_FEATURE_PLATFORM ?= u8500
+TARGET_USE_ST_ERICSSON_MULTIMEDIA := true
+
+## Build ALSA-utils
+BUILD_WITH_ALSA_UTILS := true
+BOARD_USES_ALSA := true
+
+## Build ALSA-lib
+BOARD_USES_ALSA_AUDIO := true
+
+## Below line will select Lunds ANM/ADM as AudioHardwareInterface
+BOARD_USES_LD_ANM := true
+BOARD_BYPASSES_AUDIOFLINGER_A2DP := true
+
+## PV/MediaPlayer related settings
+BUILD_PV_VIDEO_ENCODERS:=1
+BUILD_PV_2WAY:=1
+BUILD_PV_TEST_APPS:=1
+BUILD_WITHOUT_PV:=false
+
+## ENABLE STAGEFRIGHT and STE CODECS IN STAGEFRIGHT
+BUILD_WITH_FULL_STAGEFRIGHT:=true
+STE_CODECS_IN_STAGEFRIGHT:=true
+
+MULTIMEDIA_SET_PLATFORM := u8500
+HARDWARE_PATH := $(TOP)/vendor/st-ericsson/hardware
+MULTIMEDIA_PATH := $(TOP)/vendor/st-ericsson/multimedia
+KERNEL_OUTPUT := $(TOP)/kernel
--- device.mk
+++ device.mk
@@ -50,4 +50,75 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
         make_ext4fs
 
+# Include MM in the build
+
+PRODUCT_COPY_FILES += \
+        frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+        frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+        frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+        frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+        frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+        frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+        frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+        frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+        frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+        frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+        frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# Copy ALSA configuration files to rootfsdded platform feature permissions
+PRODUCT_COPY_FILES += \
+        external/alsa-lib/src/conf/alsa.conf:system/usr/share/alsa/alsa.conf \
+        external/alsa-lib/src/conf/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
+        external/alsa-lib/src/conf/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
+        external/alsa-lib/src/conf/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
+        external/alsa-lib/src/conf/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
+        external/alsa-lib/src/conf/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
+        external/alsa-lib/src/conf/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
+        external/alsa-lib/src/conf/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
+        external/alsa-lib/src/conf/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
+        external/alsa-lib/src/conf/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
+        external/alsa-lib/src/conf/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
+        external/alsa-lib/src/conf/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
+        external/alsa-lib/src/conf/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
+        external/alsa-lib/src/conf/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
+        external/alsa-lib/src/conf/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
+        external/alsa-lib/src/conf/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
+        external/alsa-lib/src/conf/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
+
+PRODUCT_PACKAGES += \
+        libasound \
+        st-ericsson-multimedia-package \
+        camera.snowball \
+        copybit.snowball \
+        overlay.snowball \
+        lights.snowball \
+        libstagefrighthw \
+        libblt_hw \
+        gralloc.snowball \
+        camera.snowball \
+        libmmhwbuffer_wrapper \
+        libhdmi_service \
+        hdmi_service_st \
+        libstagefright_ste_aacdec_sf_wrapper \
+        libaudiopolicy \
+        audio_policy.snowball \
+        audio.primary.snowball \
+        libstagefright_ste_mp3wrapper \
+        lights.snowball \
+        copybit.snowball \
+        libstagefrighthw \
+        libstagefright_soft_ste_aacdec \
+        libstagefright_soft_ste_mp3dec \
+        libaudiopolicy.so \
+        audio_policy.snowball.so \
+        audio.primary.snowball.so \
+        copybit.snowball.so \
+        gralloc.snowball.so \
+        libstagefrighthw.so \
+        libmmhwbuffer_wrapper.so
+
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
